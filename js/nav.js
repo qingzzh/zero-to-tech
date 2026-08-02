a41c583cfe867bea733b14503938c3256e806370
qingzzh <2118769896@qq.com>
2026-08-02 12:23:43 +0800

    模块化改造的补漏

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,4 @@
-(export function initNav() {
+export function initNav() {
   var path = location.pathname.split("/").pop() || "index.html";
   var links = document.querySelectorAll(".nav-link");
   for (var i = 0; i < links.length; i++) {
